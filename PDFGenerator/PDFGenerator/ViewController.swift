98e55d9a1afd853b3eaea5ac1efb432305dda3ed
Darshan Panchal <darshanpanchal@users.noreply.github.com>
2023-07-04 18:36:20 +0530

    InItial commit
    Project initial commit

---
+++ PDFGenerator/PDFGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PDFGenerator
+//
+//  Created by Darshan Panchal on 04/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PDF_Generator
 Generat PDF  
+
+## Generator PDF with Video as subview using IOS PdfKit 
